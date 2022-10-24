5dcbceed35630687b3f20b7c6166a0249a2e4266
AR-Ashik-9997 <ashikur.rahman9997@gmail.com>
2022-10-24 21:59:43 +0600

    folder structuring on my project

---
+++ src/components/Fqa/Fqa.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Fqa = () => {
+    return (
+        <div>
+            <h1>this is following question answer</h1>
+        </div>
+    );
+};
+
+export default Fqa;
