9ffc402f1b9a97101b8416071e193b524c6f0558
AR-Ashik-9997 <ashikur.rahman9997@gmail.com>
2022-10-24 22:48:21 +0600

    Setup Nabvigation bar and linking to another page

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,28 @@
-import React from 'react';
+import React from "react";
+import { Button, Container, Nav, Navbar } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const Header = () => {
-    return (
-        <div>
-            <h1>this is header page</h1>
-        </div>
-    );
+  return (
+    <div>
+      <Navbar bg="light" expand="lg">
+        <Container>
+          <Navbar.Brand as={Link} to='/' className="fs-1">Skill Platform</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <Nav.Link className="fs-5"  as={Link} to='/courses'>Courses</Nav.Link>
+              <Nav.Link className="fs-5" as={Link} to='/following-question-answer'>FAQ</Nav.Link>
+              <Nav.Link className="fs-5" as={Link} to='/blog'>Blog</Nav.Link>
+              <div className="d-flex align-items-center">
+              <Link to='/sign-in'><Button variant="outline-primary btn-md">Sign-In</Button></Link>
+              </div>                            
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </div>
+  );
 };
 
 export default Header;
